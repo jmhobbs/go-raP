efe03ccc01bd54f781e6af5fc1d34979f0b7bbaf
John Hobbs <john@velvetcache.org>
2026-08-02 15:52:34 -0500

    Move printer to subpackage

--- printer/printer.go
+++ printer/printer.go
@@ -1,43 +1,65 @@
-package raP
+package printer
 
 import (
 	"errors"
 	"fmt"
 	"io"
 	"strings"
+
+	"github.com/jmhobbs/go-raP"
 )
 
-func Printer(out io.Writer, root *File) error {
-	return PrintEntries(0, out, root.Entries)
+type Printer struct {
+	indentDepth int
 }
 
-func indent(level int, out io.Writer) {
-	for range level {
-		fmt.Fprint(out, "  ")
+type PrinterOption func(*Printer)
+
+func WithIndentDepth(depth int) PrinterOption {
+	return func(p *Printer) {
+		p.indentDepth = depth
 	}
 }
 
-func PrintEntries(indentLevel int, out io.Writer, entries []Entry) error {
+func New(opts ...PrinterOption) *Printer {
+	p := &Printer{
+		indentDepth: 2,
+	}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
+}
+
+func (p *Printer) indent(level int, out io.Writer) {
+	fmt.Fprint(out, strings.Repeat(" ", p.indentDepth*level))
+}
+
+func (p *Printer) File(out io.Writer, root *raP.File) error {
+	return p.Entries(0, out, root.Entries)
+}
+
+func (p *Printer) Entries(indentLevel int, out io.Writer, entries []raP.Entry) error {
 	for _, entry := range entries {
 		switch entry.Type() {
-		case EntryTypeClass:
-			class, ok := entry.(*Class)
+		case raP.EntryTypeClass:
+			class, ok := entry.(*raP.Class)
 			if !ok {
 				return errors.New("error: Class entry is not of type *Class")
 			}
-			PrintClass(indentLevel, out, class)
-		case EntryTypeAssignment:
-			assignment, ok := entry.(*Assignment)
+			p.Class(indentLevel, out, class)
+		case raP.EntryTypeAssignment:
+			assignment, ok := entry.(*raP.Assignment)
 			if !ok {
 				return errors.New("error: Assignment entry is not of type *Assignment")
 			}
-			PrintAssignment(indentLevel, out, assignment)
-		case EntryTypeArray:
-			array, ok := entry.(*Array)
+			p.Assignment(indentLevel, out, assignment)
+		case raP.EntryTypeArray:
+			array, ok := entry.(*raP.Array)
 			if !ok {
 				return errors.New("error: Array entry is not of type *Array")
 			}
-			PrintArray(indentLevel, out, array)
+			p.Array(indentLevel, out, array)
 		default:
 			return fmt.Errorf("unknown entry type: %d", entry.Type())
 			// todo:
@@ -50,38 +72,38 @@ func PrintEntries(indentLevel int, out io.Writer, entries []Entry) error {
 	return nil
 }
 
-func PrintClass(indentLevel int, out io.Writer, class *Class) {
-	indent(indentLevel, out)
+func (p *Printer) Class(indentLevel int, out io.Writer, class *raP.Class) {
+	p.indent(indentLevel, out)
 	fmt.Fprintf(out, "class %s", class.Name)
 	if class.InheritedClassName != "" {
 		fmt.Fprintf(out, ": %s", class.InheritedClassName)
 	}
 	fmt.Fprintln(out, "")
-	indent(indentLevel, out)
+	p.indent(indentLevel, out)
 	fmt.Fprint(out, "{\n")
-	PrintEntries(indentLevel+1, out, class.Entries)
+	p.Entries(indentLevel+1, out, class.Entries)
 	for range indentLevel {
 		fmt.Fprint(out, "  ")
 	}
 	fmt.Fprint(out, "};\n")
 }
 
-func PrintAssignment(indentLevel int, out io.Writer, assignment *Assignment) {
-	indent(indentLevel, out)
+func (p *Printer) Assignment(indentLevel int, out io.Writer, assignment *raP.Assignment) {
+	p.indent(indentLevel, out)
 	fmt.Fprintf(out, "%s = ", assignment.Name)
 	switch assignment.Subtype {
-	case AssignmentTypeString:
+	case raP.AssignmentTypeString:
 		fmt.Fprintf(out, "%q", assignment.Value)
-	case AssignmentTypeLong:
+	case raP.AssignmentTypeLong:
 		fmt.Fprintf(out, "%d", assignment.Value)
-	case AssignmentTypeFloat:
+	case raP.AssignmentTypeFloat:
 		fmt.Fprintf(out, "%f", assignment.Value)
 	}
 	fmt.Fprint(out, ";\n")
 }
 
-func PrintArray(indentLevel int, out io.Writer, array *Array) {
-	indent(indentLevel, out)
+func (p *Printer) Array(indentLevel int, out io.Writer, array *raP.Array) {
+	p.indent(indentLevel, out)
 	// special case for empty arrays
 	if len(array.Values) == 0 {
 		fmt.Fprintf(out, "%s[] = {};\n", array.Name)
@@ -92,15 +114,15 @@ func PrintArray(indentLevel int, out io.Writer, array *Array) {
 	valueIndent := strings.Repeat("  ", indentLevel+1)
 	for i, v := range array.Values {
 		switch v.Type {
-		case ArrayValueTypeString:
+		case raP.ArrayValueTypeString:
 			valuesAsStrings[i] = fmt.Sprintf("%s%q", valueIndent, v.Value)
-		case ArrayValueTypeLong:
+		case raP.ArrayValueTypeLong:
 			valuesAsStrings[i] = fmt.Sprintf("%s%d", valueIndent, v.Value)
-		case ArrayValueTypeFloat:
+		case raP.ArrayValueTypeFloat:
 			valuesAsStrings[i] = fmt.Sprintf("%s%f", valueIndent, v.Value)
 		}
 	}
 	fmt.Fprint(out, strings.Join(valuesAsStrings, ",\n")+"\n")
-	indent(indentLevel, out)
+	p.indent(indentLevel, out)
 	fmt.Fprint(out, "};\n")
 }
---
+++ printer/printer_test.go
@@ -0,0 +1,60 @@
+package printer_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/jmhobbs/go-raP"
+	"github.com/jmhobbs/go-raP/printer"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_PrintAssignment(t *testing.T) {
+	t.Run("string", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		printer.New().Assignment(
+			0,
+			&buf,
+			&raP.Assignment{
+				Name:    "example",
+				Subtype: raP.AssignmentTypeString,
+				Value:   "Hello, world!",
+			},
+		)
+
+		assert.Equal(t, `example = "Hello, world!";`+"\n", buf.String())
+	})
+
+	t.Run("float", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		printer.New().Assignment(
+			0,
+			&buf,
+			&raP.Assignment{
+				Name:    "example",
+				Subtype: raP.AssignmentTypeFloat,
+				Value:   float32(1.23),
+			},
+		)
+
+		assert.Equal(t, `example = 1.230000;`+"\n", buf.String())
+	})
+
+	t.Run("long", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		printer.New().Assignment(
+			0,
+			&buf,
+			&raP.Assignment{
+				Name:    "example",
+				Subtype: raP.AssignmentTypeLong,
+				Value:   int32(23512),
+			},
+		)
+
+		assert.Equal(t, `example = 23512;`+"\n", buf.String())
+	})
+}
